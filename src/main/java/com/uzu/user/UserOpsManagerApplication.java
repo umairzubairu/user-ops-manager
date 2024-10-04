eefbd3f37063aa2d6c24caf34e7488f1b09cabe8
umairzubairu <88110066+umairzubairu@users.noreply.github.com>
2024-10-04 19:07:17 +0500

    initial-commit

---
+++ src/main/java/com/uzu/user/UserOpsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.uzu.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserOpsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserOpsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uzu/user/UserOpsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uzu.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserOpsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
